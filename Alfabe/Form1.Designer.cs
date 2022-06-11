90c37ad827e7026baa6af50ce94e1bb7f6e204db
781enes <63649659+781enes@users.noreply.github.com>
2022-06-11 21:51:48 +0300

    Add files via upload

---
+++ Alfabe/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Alfabe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 76);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Switch Case Alfabe";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(169, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 53);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Sesli harf Sessiz harf";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kontrol et";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "Ç",
+            "D",
+            "E",
+            "F",
+            "G",
+            "Ğ",
+            "H",
+            "I",
+            "İ",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "Ö",
+            "P",
+            "R",
+            "S",
+            "Ş",
+            "T",
+            "U",
+            "Ü",
+            "V",
+            "Y",
+            "Z"});
+            this.comboBox1.Location = new System.Drawing.Point(6, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(291, 100);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Alfabe";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Alfabe/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alfabe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string harf;
+
+            harf = comboBox1.Text;
+
+            switch (harf)
+            {
+
+                case "A":
+                    {
+                        label1.Text = "A harfi 1. sıradadır.";
+                        break;
+                    }
+
+                case "B":
+                    {
+                        label1.Text = "B harfi 2. sıradadır.";
+                        break;
+                    }
+                case "C":
+                    {
+                        label1.Text = "C harfi 3. sıradadır.";
+                        break;
+                    }
+                case "Ç":
+                    {
+                        label1.Text = "Ç harfi 4. sıradadır.";
+                        break;
+                    }
+                case "D":
+                    {
+                        label1.Text = "D harfi 5. sıradadır.";
+                        break;
+                    }
+                case "E":
+                    {
+                        label1.Text = "E harfi 6. sıradadır.";
+                        break;
+                    }
+                case "F":
+                    {
+                        label1.Text = "F harfi 7. sıradadır.";
+                        break;
+                    }
+                case "G":
+                    {
+                        label1.Text = "G harfi 8. sıradadır.";
+                        break;
+                    }
+
+                case "Ğ":
+                    {
+                        label1.Text = "Ğ harfi 9. sıradadır.";
+                        break;
+                    }
+                case "H":
+                    {
+                        label1.Text = "H harfi 10. sıradadır.";
+                        break;
+                    }
+                case "I":
+                    {
+                        label1.Text = "I harfi 11. sıradadır.";
+                        break;
+                    }
+                case "İ":
+                    {
+                        label1.Text = "İ harfi 12. sıradadır.";
+                        break;
+                    }
+                case "J":
+                    {
+                        label1.Text = "J harfi 13. sıradadır.";
+                        break;
+                    }
+                case "K":
+                    {
+                        label1.Text = "K harfi 14. sıradadır.";
+                        break;
+                    }
+                case "L":
+                    {
+                        label1.Text = "L harfi 15. sıradadır.";
+                        break;
+                    }
+                case "M":
+                    {
+                        label1.Text = "M harfi 16. sıradadır.";
+                        break;
+                    }
+                case "N":
+                    {
+                        label1.Text = "N harfi 17. sıradadır.";
+                        break;
+                    }
+
+                case "O":
+                    {
+                        label1.Text = "O harfi 18. sıradadır.";
+                        break;
+                    }
+                case "Ö":
+                    {
+                        label1.Text = "Ö harfi 19. sıradadır.";
+                        break;
+                    }
+                case "P":
+                    {
+                        label1.Text = "P harfi 20. sıradadır.";
+                        break;
+                    }
+                case "R":
+                    {
+                        label1.Text = "R harfi 21. sıradadır.";
+                        break;
+                    }
+                case "S":
+                    {
+                        label1.Text = "S harfi 22. sıradadır.";
+                        break;
+                    }
+
+                case "Ş":
+                    {
+                        label1.Text = "Ş harfi 23. sıradadır.";
+                        break;
+                    }
+                case "T":
+                    {
+                        label1.Text = "T harfi 24. sıradadır.";
+                        break;
+                    }
+                case "U":
+                    {
+                        label1.Text = "U harfi 25. sıradadır.";
+                        break;
+                    }
+                case "Ü":
+                    {
+                        label1.Text = "Ü harfi 26. sıradadır.";
+                        break;
+                    }
+
+                case "V":
+                    {
+                        label1.Text = "V harfi 27. sıradadır.";
+                        break;
+                    }
+                case "Y":
+                    {
+                        label1.Text = "Y harfi 28. sıradadır.";
+                        break;
+                    }
+
+                case "Z":
+                    {
+                        label1.Text = "Z harfi 29. sıradadır.";
+                        break;
+                    }
+                default:
+                    {
+                        label1.Text = "Geçersiz harf";
+                        break;
+                    }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string harf;
+
+            harf = comboBox1.Text;
+
+            switch (harf)
+            {
+                case "a":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "e":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "ı":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "i":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "u":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "ü":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "o":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "ö":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+
+                case "A":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "E":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "I":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "İ":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "U":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "Ü":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "O":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                case "Ö":
+                    {
+                        MessageBox.Show("Sesli harf");
+                        break;
+                    }
+                default:
+                    {
+                        MessageBox.Show("Sessiz harf");
+                        break;
+                    }
+            }
+        }
+    }
+}
